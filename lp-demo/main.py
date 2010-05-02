4085fa51459d7ee7666e858b653f79c19a07957e
Alek Dembowski <alek@Alek-Dembowskis-Mac-mini.local>
2010-05-02 11:13:19 -0400

    Barebones site kind of working. Woot!

--- lp-demo/main.py
+++ lp-demo/main.py
@@ -1,33 +1,82 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
+
+import os
+
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 
+from Cheetah.Template import Template
+
+def serverPath(filename):
+  return os.path.join(os.path.dirname(__file__), filename)
+
+def loadTemplate(templatePath, searchList=[]):
+  path = serverPath(templatePath)
+  return Template(file=path, searchList = searchList)
+  
+def mergeParams(default, overrides):
+  merged = dict(default)
+  merged.update(overrides)
+  return merged
 
 class MainHandler(webapp.RequestHandler):
-    def get(self):
-        self.response.out.write('Hello world!')
+  def get(self):
+    path = serverPath('templates/index.html')
+    
+    site_header = loadTemplate('templates/site_header.html')
+    template_values = { 
+      'page_title': 'Light Pegasus',
+      'site_header': site_header,
+      'css_includes': loadTemplate('templates/css_includes.html'),
+    }
+    tmpl = loadTemplate('templates/index.html', 
+                        searchList = (template_values,))
+    self.response.out.write(tmpl)
+
+
+class PortfolioHandler(webapp.RequestHandler):
+  def getPageName(self):
+    uri = self.request.uri
+    uri = uri[uri.find('/portfolio'):]
+    return uri.replace('/portfolio', '')
+
+  def getDemoContent(self, demoName):
+    if demoName == '/':
+      return "Hello World"
+    elif demoName == '/dial':
+      return  loadTemplate('templates/dial_creator.html')
+    elif demoName == '/life':
+      return loadTemplate('templates/conways_life.html')
+    else:
+      return "Whoops, demo %s not found!" % (demoName)
+
+  def get(self):
+    commonArgs = {
+      'project_name': 'Dial Creator',
+      'page_title': 'Demos @ Light Pegasus',
+    }
+    site_header = loadTemplate(
+        'templates/site_header.html', 
+        searchList = commonArgs)
+    content = self.getDemoContent(self.getPageName())
+    template_values = mergeParams(commonArgs, { 
+      'site_header': site_header,
+      'css_includes': loadTemplate('templates/css_includes.html'),
+      'top_nav': loadTemplate('templates/top_nav.html'),
+      'page_content': content,
+      'common_js': loadTemplate('templates/common_javascript.html'),
+    })
+    tmpl = loadTemplate('templates/portfolio.html', 
+                        searchList = (template_values,))
+    self.response.out.write(tmpl)
 
 
 def main():
-    application = webapp.WSGIApplication([('/', MainHandler)],
+  application = webapp.WSGIApplication([('/portfolio/.*', PortfolioHandler),
+                                        ('/', MainHandler)],
                                          debug=True)
-    util.run_wsgi_app(application)
+  util.run_wsgi_app(application)
 
 
 if __name__ == '__main__':
-    main()
+  main()
